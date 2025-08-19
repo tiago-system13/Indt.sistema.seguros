1283ca4018d177c55da41bed996195a88e31802b
Tiago <rm353730@fiap.com.br>
2025-08-19 14:19:38 -0300

    Ordernando as parcelas geradas no contrato.

--- src/Indt.Sistema.Seguros.App.API/Contratos/ListarContratos/ContratosMapper.cs
+++ src/Indt.Sistema.Seguros.App.API/Contratos/ListarContratos/ContratosMapper.cs
@@ -7,14 +7,14 @@ namespace Indt.Sistema.Seguros.App.API.Contratos.ListarContratos
         public static ContratoDto Map(Contrato contrato)
         {
             return new ContratoDto()
-            {               
+            {
                 DataInicio = contrato.DataInicial,
                 DataFim = contrato.DataFinal,
                 Valor = contrato.Valor,
                 Prazo = contrato.Prazo,
                 Numero = contrato.Numero,
                 PropostaId = contrato.PropostaId,
-                Parcelas = contrato.Parcelas.Select(x=> Map(x)).ToList()
+                Parcelas = contrato.Parcelas.Select(x => Map(x)).OrderBy(x=> x.Numero).ToList()
             };
 
         }
